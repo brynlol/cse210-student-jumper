6fa727c8e97be7606f3933be96377f85b4851f50
brynlol <brynlol12@gmail.com>
2021-10-16 14:01:29 -0600

    added class comment

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,11 +1,26 @@
 from game.console import Console
 from game.puzzle import Puzzle
-from game.processor import Processor
+from game.jumper import Jumper
 
 class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+        keep_playing (boolean): Whether or not the game can continue.
+        console (Console): An instance of the class of objects known as Console.
+        puzzle (Puzzle): An instance of the class of objects known as Puzzle.
+        jumper (Jumper): An instance of the class of objects known as Jumper.
+    """
 
     def __init__(self):
-       pass
+        self.keep_playing = True
+        self.console = Console() 
+        self.puzzle = Puzzle()
+        self.jumper = Jumper()
     
     def start_game(self):
         self.get_inputs()
@@ -13,7 +28,8 @@ class Director:
         self.get_outputs()
 
     def get_inputs(self):
-        pass
+
+        letter = self.console.get_string("Guess a letter [a-z]: ")
 
     def do_updates(self):
         pass
