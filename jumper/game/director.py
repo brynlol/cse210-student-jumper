3aebfdaf304b40c95e7edff328274bde2dd301cf
brynlol <brynlol12@gmail.com>
2021-10-16 15:31:44 -0600

    made changes to director.py

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -11,6 +11,8 @@ class Director:
 
     Attributes:
         keep_playing (boolean): Whether or not the game can continue.
+        letter (str): The user's guess will be stored here.
+        word_line (str): The masked word will be stored here.
         console (Console): An instance of the class of objects known as Console.
         puzzle (Puzzle): An instance of the class of objects known as Puzzle.
         jumper (Jumper): An instance of the class of objects known as Jumper.
@@ -18,6 +20,8 @@ class Director:
 
     def __init__(self):
         self.keep_playing = True
+        self.letter = ''
+        self.word_line = ''
         self.console = Console() 
         self.puzzle = Puzzle()
         self.jumper = Jumper()
@@ -25,14 +29,28 @@ class Director:
     def start_game(self):
         self.get_inputs()
         self.do_updates()
-        self.get_outputs()
+        self.do_outputs()
 
     def get_inputs(self):
-
-        letter = self.console.get_string("Guess a letter [a-z]: ")
+        self.console.write_text(self.puzzle.get_word_progress())
+        
+        self.letter = self.console.get_string('Guess a letter [a-z]: ')
+        
 
     def do_updates(self):
-        pass
+        if len(self.puzzle.chosen_word) >= 3 and  len(self.puzzle.chosen_word) < 7:
+            self.jumper.num_lives = 5
+        elif len(self.puzzle.chosen_word) >= 7 and  len(self.puzzle.chosen_word) < 11:
+            self.jumper.num_lives = 7
+        else:
+            self.jumper.num_lives = 9
+        if self.puzzle.store_guess(self.letter):
+            self.word_line = self.puzzle.get_word_progress()
+        else:
+            self.jumper.lose_life() 
+        
 
-    def get_outputs(self):
-        pass
+    def do_outputs(self):
+        self.console.write_text(self.word_line)
+        self.keep_playing = (self.jumper.num_lives > 0)
+
